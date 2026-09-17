9c62d3ad8721fe0553f95354a53d1761b2be0148
Mateus Barrense Dos Santos <252016600003@ifb.local>
2026-09-17 15:49:14 -0300

    Template e Lista-funcionarios criados

--- website/urls.py
+++ website/urls.py
@@ -1,11 +1,22 @@
 from django.urls import path
+from .views import ListaFuncionarios, IndexTemplateView
+from django.views.generic import TemplateView
 
 # Importamos a função index() definida no arquivo views.py
 from . import views
 
 app_name = 'website'
 # urlpatterns contém a lista de roteamentos de URLs
+
 urlpatterns = [
     # GET /
-    path('', views.index, name='index')
+    # path('', views.index, name='index'),
+
+    path('', IndexTemplateView.as_view(), name='index'),
+
+    path('funcionarios-lista/', ListaFuncionarios.as_view(), name='lista_funcionarios'),
+
+    # path('funcionarios/', views.index, name='index'),
+
 ]
+
--- website/views.py
+++ website/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from OlaMundo.models import Funcionario
+from django.views.generic.list import ListView
+from django.views.generic import TemplateView
+
+
+
 # Create your views here.
 
 
-def index(request):
-    return lista_funcionarios(request)
+# def index(request):
+#     return lista_funcionarios(request)
 
-
+class IndexTemplateView(TemplateView):
+    template_name = "website/index.html"
 
 def lista_funcionarios(request):
     # Buscar os funcionarios
@@ -17,3 +23,10 @@ def lista_funcionarios(request):
 
     # Retornar o templete para listar os funcionarios
     return render(request, "website/funcionarios.html", contexto)
+
+
+class ListaFuncionarios(ListView):
+    template_name = "website/funcionarios-lista.html"
+    model = Funcionario
+    context_object_name = "funcionarios"
+
